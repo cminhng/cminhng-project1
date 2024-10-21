373a5005e634c6dce27ab6e65db0d20849038ba0
cminhng <christine.nguyen1@gwmail.gwu.edu>
2024-10-21 12:23:44 -0700

    completed Station (probably)

--- Station.java
+++ Station.java
@@ -2,15 +2,47 @@ public class Station {
     protected String name;
     protected String lineColor;
     protected boolean inService;
-    protected Station prevStation;
-    protected Station nextStation;
+    protected Station prev;
+    protected Station next;
 
     public Station(String name, String lineColor){
         this.name = name;
         this.lineColor = lineColor;
     }
 
-    int tripLength(Station dest){
+    public int tripLength(Station dest){
         return 0;
     }
-}
+
+    public void addNext(Station next){
+        this.next = next;
+        //next.prev = this;
+    }
+
+    public void addPrev(Station prev){
+        this.prev = prev;
+        //prev.next = this;
+    }
+
+    public void connect(Station other){
+        this.addNext(other);
+        other.addPrev(this);
+    }
+
+    public void switchAvailable(){
+        this.inService = !this.inService;
+    }
+
+    public boolean equals(Station other){
+        if(this.lineColor.equals(other.lineColor) && this.name.equals(other.name)){
+            return true;
+        }
+        return false;
+    }
+
+    public String toString(){
+        return "STATION " + name + ": " + lineColor + " line, in service: "
+        + inService + ", previous station: " + (prev == null ? "none" : next) 
+        + ", next station: " + (next == null ? "none" : next);
+    }
+}   
