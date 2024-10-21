e1757a1e4b38567b2e0077d443e1b3a988e6e7f2
cminhng <christine.nguyen1@gwmail.gwu.edu>
2024-10-21 12:23:00 -0700

    Update TransferStation.java (something went wrong with my branches so im copying directly to github)

--- TransferStation.java
+++ TransferStation.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+
 public class TransferStation extends Station {
+    static ArrayList<Station> transfers = new ArrayList<Station>();
+
     public TransferStation(String name, String lineColor){
         super(name, lineColor);
+    }
+
+    public void addTransferStationPrev(Station prev){
+        transfers.add(prev);
+    }
+
+    public String toString(){
+        return "TRANSFER" + super.toString();
     }
 }
