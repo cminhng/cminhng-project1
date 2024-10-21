3716e7d16d476048744778c860b01b17cfeb2dd4
cminhng <christine.nguyen1@gwmail.gwu.edu>
2024-10-21 12:24:13 -0700

    added makeEnd, toString

--- EndStation.java
+++ EndStation.java
@@ -1,5 +1,18 @@
 public class EndStation extends Station {
+
     public EndStation(String name, String lineColor){
         super(name, lineColor);
+    }
+
+    public void makeEnd(){
+        if(this.next != null){
+            this.prev = this.next;
+        }else if(this.prev != null){
+            this.next = this.prev;
+        }
+    };
+
+    public String toString(){
+        return "END" + super.toString();
     }
 }
